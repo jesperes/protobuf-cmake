37088ad86ad21321e4c3dda34560648eeda84d92
Wade Berrier <wberrier@gmail.com>
2013-03-04 11:14:22 -0700

    Only add file to filelist if the file exists. Implement 'make install'
    These conditional filelists help support multiple versions of the
    library.

--- protobuf-common.cmake
+++ protobuf-common.cmake
@@ -1,5 +1,8 @@
 include_directories( ${PROTOBUF_ROOT}/src )
 
+# Set relative rpath for protoc to be able to find libs after installed
+set(CMAKE_INSTALL_RPATH "\$ORIGIN/../lib")
+
 # config.h is generated from cmake now, so use on all platforms
 add_definitions( -DHAVE_CONFIG_H )
 
@@ -12,3 +15,27 @@ else()
   add_definitions( -Wno-deprecated )
 endif()
 
+# Easier to support different versions of protobufs
+function(append_if_exist OUTPUT_LIST)
+    set(${OUTPUT_LIST})
+    foreach(fil ${ARGN})
+        if(EXISTS ${fil})
+            list(APPEND ${OUTPUT_LIST} "${fil}")
+        else()
+            message("Warning: file missing: ${fil}")
+        endif()
+    endforeach()
+    set(${OUTPUT_LIST} ${${OUTPUT_LIST}} PARENT_SCOPE)
+endfunction()
+
+# Install locations
+set(BIN_DIR     bin)
+set(INCLUDE_DIR include)
+if(WIN32)
+    # On windows .dlls need to be next to the binaries
+    # TODO: We don't properly build .dlls
+    # http://www.cmake.org/Wiki/BuildingWinDLL
+    set(LIB_DIR     bin)
+else()
+    set(LIB_DIR     lib)
+endif()
